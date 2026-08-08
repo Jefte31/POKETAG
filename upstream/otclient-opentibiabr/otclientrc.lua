3b4fb65395e5239f186b85ad84210e1b0ee8d6da
Jefte31 <targinojefte@gmail.com>
2026-08-08 12:25:56 -0300

    frankenstein: configure modern OTClient for local PDA 8.54 server

--- upstream/otclient-opentibiabr/otclientrc.lua
+++ upstream/otclient-opentibiabr/otclientrc.lua
@@ -1,5 +1,19 @@
--- this file is loaded after all modules are loaded and initialized
--- you can place any custom user code here
+-- PokeTag local profile.
+-- This file is loaded after all OTClient modules are initialized.
 
-print 'Startup done :]'
+local function configurePokeTagLocal()
+  if not EnterGame then
+    return
+  end
 
+  g_settings.set('host', '127.0.0.1')
+  g_settings.set('port', 7171)
+  g_settings.set('client-version', 854)
+  g_settings.set('httpLogin', false)
+  g_settings.set('autologin', false)
+
+  EnterGame.setUniqueServer('127.0.0.1', 7171, 854)
+  EnterGame.setHttpLogin(false)
+end
+
+addEvent(configurePokeTagLocal)
